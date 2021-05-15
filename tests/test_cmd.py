b94247f52f84df66a166c2a5ff2f185470b06afd
claudiolau <claudio.lau12@gmail.com>
2021-05-15 17:48:51 -0400

    added pytest

--- tests/test_cmd.py
+++ tests/test_cmd.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 
-import unittest
+import pytest
 from click.testing import CliRunner
 from mrpeace import cli
 
 
-class TestCommand(unittest.TestCase):
-    def test_callback(self):
-        runner = CliRunner()
-        result = runner.invoke(cli.hi, ["hi"])
-        assert result.exit_code == 2
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_callback():
+    runner = CliRunner()
+    result = runner.invoke(cli.hi, ["hi"])
+    assert result.exit_code == 2
